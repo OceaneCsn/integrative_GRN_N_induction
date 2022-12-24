cae692f5959eec5abe25b386c97f44be66c7efe5
Oceane Cassan <oceane.cssn@gmail.com>
2022-12-24 11:42:03 +0100

    added gene annotations and link function plots

---
+++ inference_functions/show_link_functions.R
@@ -0,0 +1,35 @@
+library(tidyr)
+library(ggplot2)
+library(ggpubr)
+
+ALPHAS <- seq(0,1, by = 0.1)
+
+
+weights_rf <- function(pi, alpha){
+  if (pi == 1)
+    return(1)
+  if(pi==0.5)
+    return(1-alpha)
+  if(pi == 0)
+    return(-sqrt(1-(alpha-1)^2)+1)
+}
+
+
+weights_lasso <- function(pi, alpha){
+  return(1 - pi * alpha)
+}
+
+
+links <- data.frame("Pi" = rep(c(1, 0.5, 0), each = 11),
+                    "alpha" = rep(ALPHAS, 3),
+                    "bRF" = mapply(weights_rf, rep(c(1, 0.5, 0), each = 11), rep(ALPHAS, 3)),
+                    "LASSO-D3S" = mapply(weights_lasso, rep(c(1, 0.5, 0), each = 11), rep(ALPHAS, 3)))
+
+links %>%
+  gather(key = "method", value = "weight", bRF, LASSO.D3S) %>%
+  ggplot(aes(x=alpha, y=weight, color = factor(Pi))) + 
+  geom_point() +
+  facet_wrap(~method) + 
+  geom_smooth() +
+  theme_pubr()
+
