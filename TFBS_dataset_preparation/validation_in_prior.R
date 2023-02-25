44ceb6c04fdd381b7956103286e5ada4fe6e5b3a
Oceane Cassan <oceane.cssn@gmail.com>
2023-02-25 21:04:31 +0100

    corrected prior validation

--- TFBS_dataset_preparation/validation_in_prior.R
+++ TFBS_dataset_preparation/validation_in_prior.R
@@ -7,6 +7,7 @@ load(file = "rdata/inference_input_N_response_varala.rdata")
 load("rdata/pwm_occurrences_N_response_varala.rdata")
 
 genes <- input_data$grouped_genes
+tfs <- input_data$grouped_regressors
 known_tfs <- unique(pwm_prom$TF)
 known_tfs <- intersect(known_tfs, colnames(pwm_occurrence))
 
@@ -16,24 +17,41 @@ dim(pwm_occurrence)
 # number of edges of the PWM network
 sum(pwm_occurrence[,known_tfs])
 
-links <- getLinkList(pwm_occurrence[,known_tfs], reportMax = sum(pwm_occurrence[,known_tfs]))
+links <- getLinkList(t(pwm_occurrence[,known_tfs]), reportMax = sum(pwm_occurrence[,known_tfs]))
 colnames(links)[1:2] <- c('from', 'to')
 head(links)
 
-evaluate_network(links, input_genes = genes, input_tfs = colnames(pwm_occurrence))[c("tpr", "recall")]
-evaluate_network(links, input_genes = genes, input_tfs = colnames(pwm_occurrence), validation = c("CHIPSeq"))[c("tpr", "recall")]
-evaluate_network(links, input_genes = genes, input_tfs = colnames(pwm_occurrence), validation = c("TARGET"))[c("tpr", "recall")]
-# evaluate_network(links, input_genes = genes, input_tfs = colnames(pwm_occurrence), validation = c("DAPSeq"))[c("tpr", "recall")]
-
-val_conn$tpr
-val_conn$recall
-
-val_chip <- evaluate_network(links, input_genes = genes, input_tfs = colnames(pwm_occurrence))
-val_conn$tpr
-val_conn$recall
+evaluate_network(links, input_genes = genes, input_tfs = known_tfs)[c("tpr", "recall")]
+evaluate_network(links, input_genes = genes, input_tfs = known_tfs, validation = c("CHIPSeq"))[c("tpr", "recall")]
+evaluate_network(links, input_genes = genes, input_tfs = known_tfs, validation = c("TARGET"))[c("tpr", "recall")]
+evaluate_network(links, input_genes = genes, input_tfs = known_tfs, validation = c("DAPSeq"))[c("tpr", "recall")]
 
 
-network <- graph_from_data_frame(links, directed = T)
-edges <- as_long_data_frame(network)[c(4,5)]
 
-bRF_network(pwm_occurrence)
+valdap <- validated_edges[stringr::str_detect(validated_edges$type, "DAPSeq"),]
+intersect(genes, unique(valdap$to))
+intersect(tfs, unique(valdap$from))
+studied_tfs <- unique(valdap$from)
+
+both <- intersect(known_tfs, studied_tfs)
+
+setdiff(known_tfs, studied_tfs)
+
+tf <- "AT4G31800"
+
+validation_fraction <- function(tf){
+  return(length(setdiff(valdap[valdap$from==tf,"to"],
+                names(which(pwm_occurrence[,tf] == 1))))
+         / length(names(which(pwm_occurrence[,tf] == 1))))
+}
+
+sapply(both, FUN = validation_fraction)
+
+hist(sapply(both, FUN = validation_fraction), breaks = 100)
+
+
+
+nrow(merge(links, valdap, by = c('from', 'to')))/sum(links$from %in% studied_tfs)
+
+
+valdap[val_dap$from %in% known_tfs, ]
