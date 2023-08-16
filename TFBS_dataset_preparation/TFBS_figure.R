09c61bfafe381c1bd91db305145dae3a47c67242
OceaneCsn <oceane.cssn@gmail.com>
2023-08-16 14:48:04 +0200

    corrected prior TFBM supp figure

--- TFBS_dataset_preparation/TFBS_figure.R
+++ TFBS_dataset_preparation/TFBS_figure.R
@@ -17,14 +17,14 @@ known_tfs <- intersect(pwm_prom$TF, tfs)
 
 counts <- table(pwm_prom_$target)
 mot_promN <- ggplot(data.frame(counts), aes(x =Freq)) + 
-  geom_histogram(fill = "#8497B0", alpha=1) + ggtitle("Number of distinct TFBS per promoter") +
+  geom_histogram(fill = "#8497B0", alpha=1) + ggtitle("Number of distinct TFBM per promoter") +
   labs(subtitle ="Among the 70 nitrate responsive TFs with a PWM")+ xlab("") + ylab("Count") + 
   geom_vline(xintercept = mean(counts), col = rgb(174/255, 90/255,30/255), size = 2)+
   theme_pubr()
 
 counts <- table(pwm_prom_$TF)
 cib_promN <- ggplot(data.frame(counts), aes(x =Freq)) + 
-  geom_histogram(fill = "gray", alpha=1) + ggtitle("Number of distinct target TFBS per TF")+
+  geom_histogram(fill = "gray", alpha=1) + ggtitle("Number of distinct target TFBM per TF")+
   labs(subtitle = "In the promoters of the 1426 nitrate responsive genes") + xlab("") + ylab("Count") + 
   geom_vline(xintercept = mean(counts), col = rgb(174/255, 90/255,30/255), size = 2) +
   theme_pubr()
